417f3b1485e71c66dce5e088e0b1332ab41c8789
EC2121 <eliacantinipp@gmail.com>
2022-11-13 02:36:57 +0100

    Code Refactoring

--- Soruce/FAT16Interpreter.py
+++ Soruce/FAT16Interpreter.py
@@ -27,26 +27,42 @@ class Fat16Interpreter:
         self.read_root_dir(full_disk)
         disk_image.close()
 
+    def match_file_type(self, file_type, curr_cluster, curr_offset):
+        entry_name = curr_cluster[curr_offset: 11 + curr_offset].decode("utf-8")
+        match file_type:
+            case '0x0':
+                return  # null
+            case '0x8':  # disk name
+                self.disk_name = entry_name
+                print('disk name: ' + entry_name)
+            case '0x10':  # folder
+                print(' folder ' + entry_name.rstrip() + ':')
+                # get the cluster index of the folder and read the FAT
+                cluster_offset = curr_offset + 26
+                cluster_index = struct.unpack("<H", curr_cluster[cluster_offset: cluster_offset + 2])[0]
+                self.read_file_allocation_table(cluster_index, True)
+            case '0x20':  # file
+                cluster_offset = curr_offset + 26
+                cluster_index = struct.unpack("<H", curr_cluster[cluster_offset: cluster_offset + 2])[
+                    0]  # get cluster index
+                file_name_offset = curr_offset
+                file_ext_offset = 8 + curr_offset
+                file_name = curr_cluster[file_name_offset: 8 + file_name_offset].decode(
+                    "utf-8").rstrip()  # get file name
+                file_ext = curr_cluster[file_ext_offset: file_ext_offset + 3].decode("utf-8")  # get file ext
+                print('     ' + file_name + '.' + file_ext + ':')
+                self.read_file_allocation_table(cluster_index,
+                                                False)  # read again the FAT this time is_folder is false
+
     def read_root_dir(self, full_disk):
         # Entire root directory
         root_directory = full_disk[self.root_directory_start_index: self.clusters_start_index]
         # cycle all the directory entries
         for i in range(self.dir_entries):
             # read the file type and name
-            file_type = hex(root_directory[11 + i * self.dir_entry_size])
-            entry_name = root_directory[i * self.dir_entry_size: 11 + (i * self.dir_entry_size)].decode("utf-8")
-            match file_type:
-                case '0x0':  # null
-                    continue
-                case '0x8':  # disk name
-                    self.disk_name = entry_name
-                    print('disk name: ' + entry_name)
-                case '0x10':  # folder
-                    print(' folder ' + entry_name.rstrip() + ':')
-                    # get the cluster index of the folder and read the FAT
-                    cluster_offset = (i * self.dir_entry_size) + 26
-                    cluster_index = struct.unpack("<H", root_directory[cluster_offset: cluster_offset + 2])[0]
-                    self.read_file_allocation_table(cluster_index, True)
+            curr_offset = i * self.dir_entry_size
+            file_type = hex(root_directory[11 + curr_offset])
+            self.match_file_type(file_type, root_directory, curr_offset)
 
     def read_file_allocation_table(self, starting_cluster_index, is_folder):
         # read the FAT at the given index
@@ -61,28 +77,15 @@ class Fat16Interpreter:
             self.create_linked_list(curr_node_index)
 
     def read_cluster(self, curr_node_index, is_folder):
-        starting_index = self.clusters_start_index + (512 * (curr_node_index - 2))
+        starting_index = self.clusters_start_index + (self.sector_size * (curr_node_index - 2))
         if is_folder:
             starting_index += self.dir_entry_size * 2  # we know it is a folder so we can skip the first 64 bytes
             curr_cluster = self.full_disk[
                            starting_index: starting_index + self.sector_size - 64]  # we get the curr cluster( minus the first 64)
             for i in range(len(curr_cluster) // self.dir_entry_size):  # read every entry
-                file_type = curr_cluster[11 + i * self.dir_entry_size]  # get file type
-                match hex(file_type):
-                    case '0x0':
-                        continue
-                    case '0x20':
-                        cluster_offset = (i * self.dir_entry_size) + 26
-                        cluster_index = struct.unpack("<H", curr_cluster[cluster_offset: cluster_offset + 2])[
-                            0]  # get cluster index
-                        file_name_offset = i * self.dir_entry_size
-                        file_ext_offset = 8 + i * self.dir_entry_size
-                        file_name = curr_cluster[file_name_offset: 8 + file_name_offset].decode(
-                            "utf-8").rstrip()  # get file name
-                        file_ext = curr_cluster[file_ext_offset: file_ext_offset + 3].decode("utf-8")  # get file ext
-                        print('     ' + file_name + '.' + file_ext + ':')
-                        self.read_file_allocation_table(cluster_index,
-                                                        False)  # read again the FAT this time is_folder is false
+                curr_offset = i * self.dir_entry_size
+                file_type = curr_cluster[11 + curr_offset]  # get file type
+                self.match_file_type(hex(file_type), curr_cluster, curr_offset)
         else:  # it's a file and we can read it
             curr_cluster = self.full_disk[starting_index: starting_index + self.sector_size]
             print("         " + curr_cluster[0: curr_cluster.index(10)].decode("utf-8"))
@@ -113,4 +116,4 @@ class Fat16Interpreter:
 
 
 fat16Int = Fat16Interpreter()
-fat16Int.read_disk_image("test.img")
+fat16Int.read_disk_image("Soruce/test.img")
